a0e463333c848bdfbdc9be1e1a9aa6856ec45c9b
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-16 00:52:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {}, 
       :matriarch => {}, 
       :hero => {}, 
-      :her_friends => {}
+      :hero_friends => {}
       #your key/value pairs here!
       }, 
     :capulet => {
